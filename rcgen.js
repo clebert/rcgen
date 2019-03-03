de5eae2c99f4dd07a62d165e76b9e3b7f330f4c5
Clemens Akens <clebert@me.com>
2019-03-03 20:28:51 +0100

    build(all): generate .node-version file using rcgen

--- rcgen.js
+++ rcgen.js
@@ -1,6 +1,23 @@
 // @ts-check
 
 /**
+ * @type {import('@rcgen/core').Filetype<string[]>}
+ */
+const textFiletype = {
+  contentSchema: {type: 'array', items: {type: 'string'}},
+  serializer: content => Buffer.from(content.join('\n')),
+  deserializer: contentData => contentData.toString().split('\n')
+};
+
+/**
  * @type {import('@rcgen/core').Manifest}
  */
-exports.default = {files: []};
+exports.default = {
+  files: [
+    {
+      filename: '.node-version',
+      filetype: textFiletype,
+      initialContent: ['10', '']
+    }
+  ]
+};
