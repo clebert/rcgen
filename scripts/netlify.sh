27a4dbd01bcde2d7eab12ade3b0a550bdef22ef3
Clemens Akens <clebert@me.com>
2019-03-07 14:05:11 +0100

    build(all): setup redirect to github for rcgen.io

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -6,3 +6,4 @@ yarn install
 rm -rf docs
 $(npm bin)/lerna exec --no-private --parallel -- typedoc --options ../../typedoc.js --out ../../docs/\$LERNA_PACKAGE_NAME --tsconfig tsconfig.json .
 node scripts/process-docs.js
+echo '/ https://github.com/clebert/rcgen 302' > docs/_redirects
